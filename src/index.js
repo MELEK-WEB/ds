7ba9583618a7f2bd0541a180aa20fca09038df3d
GanikasSa <61113449+MELEK-WEB@users.noreply.github.com>
2023-05-09 23:05:17 +0100

    changess

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+            <Route  path='/hello' element={<App/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ src/utils.js
@@ -0,0 +1,12 @@
+
+
+// function  hundlSubmit (e){
+// e.preventDefault();
+
+// }
+
+// <form onSubmit={hundlSubmit}>
+//     <label></label>
+//     <input></input>
+
+// </form>
