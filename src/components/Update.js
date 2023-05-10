8d21196e78165ecd3e728a903484195f6a6ad1a7
GanikasSa <61113449+MELEK-WEB@users.noreply.github.com>
2023-05-10 09:44:54 +0100

    Recips List working

---
+++ src/components/Update.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useParams } from "react-router-dom";
+
+export default function Update() {
+    const { id } = useParams();
+
+  return (
+    <div>
+
+hello
+
+    </div>
+  )
+}
