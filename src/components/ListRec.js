3863587627d128c6491cc8b564f3f70efd478e69
GanikasSa <61113449+MELEK-WEB@users.noreply.github.com>
2023-05-10 10:24:12 +0100

    show steps

--- src/components/ListRec.js
+++ src/components/ListRec.js
@@ -1,40 +1,52 @@
 import React, { useEffect, useState } from "react";
 import Filter from "./Filter";
-import { fetchIngredients, fetchRecipes } from "../recipes.service";
+import { fetchRecipes } from "../recipes.service";
 import { Link } from "react-router-dom";
 
 export default function ListRec() {
   const [RecList, setRecList] = useState([]);
-
-
-
+  
   useEffect(() => {
-     fetchRecipes().then((data)=>{
-        setRecList(data)
-     });
-   
+    fetchRecipes().then((data) => {
+      setRecList(data);
+    });
   }, []);
 
-
-  if(RecList.length==0) {
-    return <h4>Loading ..........</h4>
-  }
   return (
     <div>
       <h1>1ère page</h1>
 
       {RecList.map((Rec) => {
-        return <div key={Rec?.id} style={{width:400,height:400,border:"1px solid black",marginLeft:50}}>
-
-<Link to={`/update/${Rec?.id}`}>Update</Link>
-
-                <h5>{Rec?.title}</h5>
-                <h5>{Rec?.category}</h5>
-                <h5 style={{cursor:"pointer"}}> show steps</h5>
-                 </div>;
+        return <RecipeCard key={Rec?.id} recipe={Rec} />;
       })}
 
       <Filter />
+    </div>
+  );
+}
+
+function RecipeCard({ recipe }) {
+  const [showSteps, setShowSteps] = useState(false);
+
+  function toggleSteps() {
+    setShowSteps(!showSteps);
+  }
+
+  return (
+    <div style={{ width: 400, height: 400, border: "1px solid black", marginLeft: 50 }}>
+      <Link to={`/update/${recipe?.id}`}>Update</Link>
+      <h5>{recipe?.title}</h5>
+      <h5>{recipe?.category}</h5>
+      <h5 style={{ cursor: "pointer" }} onClick={toggleSteps}>
+        {showSteps ? "Hide steps" : "Show steps"}
+      </h5>
+      {showSteps && (
+        <ul>
+          {recipe?.steps?.map((step, index) => (
+            <li key={index}>{step}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
